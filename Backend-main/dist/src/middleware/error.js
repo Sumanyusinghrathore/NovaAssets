948004c8010065649622993a02bfd4578bd20640
Sumanyu Singh Rathore <sumanyusinghr@gmail.com>
2026-07-12 10:37:44 +0530

    Ignore node_modules and build artifacts

--- Backend-main/dist/src/middleware/error.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = errorHandler;
-function errorHandler(err, _req, res, _next) {
-    console.error("Unhandled error:", err);
-    return res.status(500).json({ success: false, message: "Internal server error", error: err.message, stack: err.stack });
-}
-;
--- Backend-main/dist/src/utils/id.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateId = generateId;
-function generateId() {
-    return Math.random().toString(36).substring(2) + Date.now().toString(36);
-}
